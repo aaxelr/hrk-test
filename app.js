75c9c97662eb7e2667ebe007d16f0ea477e3be0d
Antonio A R <antonio.axelsson@gmail.com>
2021-03-12 12:03:27 +0100

    fix typo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const e = require("express");
 const app = e();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.end('<h1>hello wörld</h1>');
 });
